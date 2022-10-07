e6f2367e5e1fc4ce94c43da33f6112b08f9a8dde
Leandro <lafs.dev@gmail.com>
2022-10-07 00:26:56 -0300

    Temp

---
+++ AgendaSalas/AgendaSalas/Context/APIDbContext.cs
@@ -0,0 +1,19 @@
+﻿using AgendaSalas.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AgendaSalas.Context
+{
+    public class APIDbContext : DbContext
+    {
+        public APIDbContext(DbContextOptions<APIDbContext> options) : base(options)
+        {
+
+        }
+        
+        public DbSet<Cliente> Cliente { get; set; }
+
+        public DbSet<Endereco> Endereco { get; set; }
+
+    }
+}
---
+++ AgendaSalas/AgendaSalas/Migrations/20221007031649_Migration_01.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgendaSalas.Migrations
+{
+    public partial class Migration_01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    ClienteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(240)", maxLength: 240, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Celular = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cpf = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rg = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.ClienteId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Endereco",
+                columns: table => new
+                {
+                    EnderecoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Logradouro = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Bairro = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Numero = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Complemento = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Endereco", x => x.EnderecoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cliente");
+
+            migrationBuilder.DropTable(
+                name: "Endereco");
+        }
+    }
+}
